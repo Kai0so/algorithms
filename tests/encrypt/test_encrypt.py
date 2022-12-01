7f0ee0c7e496df4ad18a9deca359d34daeb172dc
Kai0so <kaioaugustoalmeida.m@gmail.com>
2022-12-01 03:29:02 -0300

    test: Implementa testes para a função encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,15 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    result = encrypt_message("xablau", 3)
+    assert result == "bax_ual"
+
+    result = encrypt_message("xablau", 8)
+    assert result == "ualbax"
+
+    with pytest.raises(TypeError):
+        encrypt_message("xablau", "8")
+        encrypt_message(5, 3)
+        encrypt_message(3, "8")
