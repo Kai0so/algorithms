6847724e96bdcfea78517e7b1fd63ef766f90eef
Kai0so <kaioaugustoalmeida.m@gmail.com>
2022-12-01 14:41:36 -0300

    feat: Implementa função is_palindrome_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,9 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+    if high_index - low_index <= 2 and word[low_index] == word[high_index]:
+        return True
+    else:
+        return word[low_index] == word[high_index] and is_palindrome_recursive(
+            word, low_index + 1, high_index - 1
+        )
