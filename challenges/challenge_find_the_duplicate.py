d1df1f825781bc4ceb3f6f303cf3521cccbf6d2f
Kai0so <kaioaugustoalmeida.m@gmail.com>
2022-12-14 20:56:32 -0300

    feat: Implementa função find_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,10 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    nums.sort()
+    n = len(nums)
+    is_duplicate = -1
+    for index in range(0, n - 1):
+        if nums[index] == nums[index + 1]:
+            is_duplicate = nums[index]
+    if is_duplicate < 0:
+        return False
+    return is_duplicate
